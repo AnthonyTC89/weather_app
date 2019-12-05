479f9b3b9fafb40162493e637416a4758e6fcab2
AnthonyTC89 <ptonyptc@gmail.com>
2019-12-05 14:03:16 -0500

    basic bootstrap installed

--- src/dom.js
+++ src/dom.js
@@ -1,13 +1,20 @@
 const setupHeader = async () => {
   console.log('Header')
+  const header = document.querySelector('header');
+  const h1 = document.createElement('h1');
+  h1.classList.toggle('main-header');
+  h1.textContent = 'Weather App';
+  header.appendChild(h1);
 };
 
 const setupMain = async () => {
   console.log('Main')
+  const main = document.querySelector('main');
 };
 
 const setupFooter = async () => {
   console.log('Footer')
+  const footer = document.querySelector('footer');
 };
 
 export {setupHeader, setupMain, setupFooter}
--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 import {setupHeader, setupMain, setupFooter} from './dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './style/style.css';
 
 Promise.all([setupHeader(),setupMain(),setupFooter()])
   .then(result => console.log(result))
   .catch(error => console.log(`Error in executing ${error}`));
+
