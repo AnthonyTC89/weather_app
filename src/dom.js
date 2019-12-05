51d88d58a87313e949c0fbfe57c8886b65eea758
AnthonyTC89 <ptonyptc@gmail.com>
2019-12-05 15:23:45 -0500

    basic header, main and footer done

--- src/dom.js
+++ src/dom.js
@@ -1,20 +1,55 @@
 const setupHeader = async () => {
-  console.log('Header')
   const header = document.querySelector('header');
+  header.classList.add('container');
+
   const h1 = document.createElement('h1');
-  h1.classList.toggle('main-header');
+  h1.classList.add('main-header', 'text-center','py-3');
   h1.textContent = 'Weather App';
   header.appendChild(h1);
 };
 
 const setupMain = async () => {
-  console.log('Main')
   const main = document.querySelector('main');
+  main.classList.add('container');
+
+  const inputSection = document.createElement('section');
+  inputSection.classList.add('input-section','row');
+
+  const inputCity = document.createElement('input');
+  inputCity.type = 'text';
+  inputCity.placeholder = 'Enter a City';
+  
+  const submitBtn = document.createElement('button');
+  submitBtn.classList.add('btn', 'btn-success');
+  submitBtn.type = 'button';
+  submitBtn.textContent = 'Search';
+  submitBtn.addEventListener('click', () => { 
+    console.log(inputCity.value)
+  });
+  inputSection.appendChild(inputCity);
+  inputSection.appendChild(submitBtn);
+
+  const weatherSection = document.createElement('section');
+  weatherSection.classList.add('weather-section', 'row');
+
+  main.appendChild(inputSection);
+  main.appendChild(weatherSection);
 };
 
 const setupFooter = async () => {
-  console.log('Footer')
   const footer = document.querySelector('footer');
+  footer.classList.add('container','text-right');
+
+  const spanFooter = document.createElement('span');
+  spanFooter.textContent = 'Powered by ';
+
+  const linkOpWe = document.createElement('a');
+  linkOpWe.textContent = 'OpenWeather';
+  linkOpWe.href = 'https://openweathermap.org/api';
+  linkOpWe.target = '_blank';
+
+  spanFooter.appendChild(linkOpWe);
+  footer.appendChild(spanFooter);
 };
 
 export {setupHeader, setupMain, setupFooter}
